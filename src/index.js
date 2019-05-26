632f0da85e56c30a4d64a0c2e7327a124dc590ac
Derek Ericson <derek.ericson@gmail.com>
2019-05-25 19:00:09 -0700

    Move app to root

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+// import User from './User';
+
+// var passport = require('passport');
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+// passport.use(new GoogleStrategy({
+//     clientID: '',
+//     clientSecret: '',
+//     callbackURL: "http://www.example.com/auth/google/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//          return done(err, user);
+//        });
+//   }
+// ));
+
+ReactDOM.render(
+            <BrowserRouter>
+              <App/>
+            </BrowserRouter >
+          ,
+          document.getElementById('root')
+        );
+
+registerServiceWorker();
